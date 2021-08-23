5badba385a72a4b4cb29d01606a0190eb2bc7928
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2021-08-23 14:35:11 +0530

    mei gallery made responsive

--- src/app/main/home/media-gallery-and-download/media-gallery-and-download.component.ts
+++ src/app/main/home/media-gallery-and-download/media-gallery-and-download.component.ts
@@ -1,30 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 
-export const ITEMS: any = [
+export const Gallery: any = [
   {
-    title: 'Our Mission',
+    title: 'Contractors Meet',
     value: 'item_1',
     message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
-    img_url: '../../../../assets/sample/1.png',
-    img_discription: 'Tylax Our Mission Image'
+    images: [
+      {
+      img_url: '../../../../assets/sample/1.png',
+      img_discription: 'Tylax Image Gallery'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      }]
   },
   {
-    title: 'Title 2',
+    title: 'Awards & Recognition',
     value: 'item_2',
-    message: 'Title 2 message',
-    img_url: '../../../../assets/sample/1.png'
+    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    images: [
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Image Gallery'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      }]
   },
   {
-    title: 'Title 3',
+    title: 'Projects Done',
     value: 'item_3',
-    message: 'Title 3 message',
-    img_url: '../../../../assets/sample/1.png'
+    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    images: [
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Image Gallery'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      }]
   },
   {
-    title: 'Title 4',
+    title: 'Exhibitions',
     value: 'item_4',
-    message: 'Title 4 message',
-    img_url: '../../../../assets/sample/1.png'
+    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    images: [
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Image Gallery'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      },
+      {
+        img_url: '../../../../assets/sample/1.png',
+        img_discription: 'Tylax Our Mission Image'
+      }]
   },
 ];
 
@@ -37,14 +84,14 @@ export const ITEMS: any = [
 export class MediaGalleryAndDownloadComponent implements OnInit {
 
 
-  radioSel: any;
-  radioSelected?: string;
-  radioSelectedString?: string;
-  itemsList: any = ITEMS;
+  selectItem: any;
+  selected?: string;
+  selectedString?: string;
+  galleryList: any = Gallery;
 
   constructor() {
-    this.itemsList = ITEMS;
-    this.radioSelected = 'item_1';
+    this.galleryList = Gallery;
+    this.selected = 'item_1';
     this.getSelecteditem();
   }
 
@@ -52,8 +99,11 @@ export class MediaGalleryAndDownloadComponent implements OnInit {
   }
 
   getSelecteditem(): any {
-    this.radioSel = ITEMS.find((Item: any) => Item.value === this.radioSelected);
-    this.radioSelectedString = JSON.stringify(this.radioSel);
+    this.selectItem = Gallery.find((Item: any) => Item.value === this.selected);
+    this.selectedString = JSON.stringify(this.selectItem);
+    console.log(this.selectItem);
+    console.log(this.selectedString);
+
   }
 
   onItemChange(item: any): any {

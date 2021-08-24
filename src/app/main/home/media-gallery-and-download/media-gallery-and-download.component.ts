0a50a79bc87de56662e7080e24d11aa43309e305
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2021-08-24 14:38:46 +0530

    media and gallery issues resolved

--- src/app/main/home/media-gallery-and-download/media-gallery-and-download.component.ts
+++ src/app/main/home/media-gallery-and-download/media-gallery-and-download.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { concat } from 'rxjs';
 
 export const Gallery: any = [
   {
     id: 'item_1',
     title: 'Contractors Meet',
-    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    message: '1 Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
     images: [
       {
-        id: 'image_1',
+        id: 'image_1_1',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_2',
+        id: 'image_1_2',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_3',
+        id: 'image_1_3',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       }]
@@ -26,20 +26,20 @@ export const Gallery: any = [
   {
     id: 'item_2',
     title: 'Awards & Recognition',
-    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    message: '2 Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
     images: [
       {
-        id: 'image_1',
+        id: 'image_2_1',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_2',
+        id: 'image_2_2',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_3',
+        id: 'image_2_3',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       }]
@@ -47,20 +47,20 @@ export const Gallery: any = [
   {
     id: 'item_3',
     title: 'Projects Done',
-    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    message: '3 Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
     images: [
       {
-        id: 'image_1',
+        id: 'image_3_1',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_2',
+        id: 'image_3_2',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_3',
+        id: 'image_3_3',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       }]
@@ -68,20 +68,20 @@ export const Gallery: any = [
   {
     id: 'item_4',
     title: 'Exhibitions',
-    message: 'Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
+    message: '4 Construction Chemicals plays a important role, it helps in increasing the productivity but also contribute in green building solutions, which are safe for environment. Construction chemical industry is a emerging enterprises in India. By maintaining the global standard and keep latest innovations in technology to challenge traditional methods in construction',
     images: [
       {
-        id: 'image_1',
+        id: 'image_4_1',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_2',
+        id: 'image_4_2',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       },
       {
-        id: 'image_3',
+        id: 'image_4_3',
         img_url: '../../../../assets/sample/mediaGallery1.jpg',
         img_discription: 'Tylax Image Gallery'
       }]
@@ -94,11 +94,8 @@ export const Gallery: any = [
   templateUrl: './media-gallery-and-download.component.html',
   styleUrls: ['./media-gallery-and-download.component.scss']
 })
-export class MediaGalleryAndDownloadComponent implements OnInit {
-
-
+export class MediaGalleryAndDownloadComponent implements OnInit, AfterViewInit {
   selectedItem: any;
-  selectedString?: string;
   galleryList: any = Gallery;
 
   constructor() {
@@ -109,29 +106,28 @@ export class MediaGalleryAndDownloadComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getSelecteditem(item: any): void {
+  ngAfterViewInit(): void {
+    this.clickSelectActiveItem('item_id_', 'item_1');
+  }
+
+  getSelecteditem(item: string): void {
     this.selectedItem = Gallery.find((Item: any) => Item.id === item);
-    this.selectedString = JSON.stringify(this.selectedItem);
-    console.log(this.selectedItem);
-    console.log(this.selectedString);
+    let imageId: string = this.selectedItem?.images[0]?.id;
+    imageId = 'image_id_' + imageId;
+    setTimeout(() => {
+      this.hoverSelectActiveImg(imageId);
+    }, 200);
   }
 
-  setActiveClass(selectedItemId?: Event, selectedImageId?: Event): void{}
-
-  onItemChange(item: any): any {
-    this.getSelecteditem(item);
-  }
-
-  hoverSelectActiveImg(selectedImageId: any): any{
-    const id: string = '#' + selectedImageId;
-    console.log(id);
+  hoverSelectActiveImg(selectedImageId: string): void{
     document.querySelector('.cards .cards-item img.active')?.classList.remove('active');
-    document.getElementById(id)?.classList.add('active');
-    // event.path[0]?.classList.add('active');
+    document.getElementById(selectedImageId)?.classList.add('active');
   }
 
-
-  a(e: Event): any{
-    console.log(e);
+  clickSelectActiveItem(selectedItem: string, selectedItemId: string): void {
+    const itemSelected: string = selectedItem + selectedItemId;
+    document.querySelector('.data-target .item-list .item.active')?.classList.remove('active');
+    document.getElementById(itemSelected)?.classList.add('active');
+    this.getSelecteditem(selectedItemId);
   }
 }
